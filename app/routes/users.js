51ca92aa82ab2eb628d53c53303db1fd8f1401c6
Flávia Thaís Pedroso <flaviathp@gmail.com>
2022-02-02 15:32:06 -0300

    autenticação registrando o usuário

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/user');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// #region Registro de usuários
+
+// rota para cadastro de usuários
+router.post('/register', async(req, res) => {
+  // requisição que retorna as informações do usuário
+  const { name, email, password } = req.body;
+  // cria um novo usuário
+  const user = new User({ name, email, password });
+
+  try {
+    // caso dê certo salvar retorna um status de sucesso e um json com as informoções do usuário
+    await user.save();
+    res.status(200).json(user);
+  } catch (error) {
+    // caso dê certo salvar retorna um status de erro e um json com as informoções do usuário
+    res.status(500).json({ error: 'Error registring new user' });
+  }
 });
+
+// #endregion
 
 module.exports = router;
